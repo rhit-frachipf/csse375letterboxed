df96e94456917c078966b9c4ad4ed22d0fc04fb3
rhit-frachipf <143839359+rhit-frachipf@users.noreply.github.com>
2026-03-13 08:52:34 -0400

    Add files via upload
    Added Real Original Project

---
+++ httpserver.py
@@ -0,0 +1,183 @@
+import flask
+import json
+import os
+import pickledb
+
+app = flask.Flask(__name__,             # ie "http_server_starter"
+            static_url_path='', 	    # Treat all files as static files.
+            static_folder='public')	    # Look in the public folder.
+
+db = pickledb.load("users.db", auto_dump=True)
+list_name = "usersList"
+users = []
+app.username = ""
+app.password = ""
+app.watched = {}
+app.watchlist = []
+app.loggedin = False
+
+if not db.get(list_name):
+    db.lcreate(list_name)
+
+def signup():
+    for x in db.getall():
+        users = db.get(x)
+        usernamelist = []
+        for y in users:
+            usernamelist.append(y.get("username"))
+        if (app.username in usernamelist):
+            print("User already exists")
+            break
+        else:
+            entry = {"username": app.username,
+                     "password": app.password,
+                     "watched": {},
+                     "whattowatch": []}
+            db.ladd(list_name, entry)
+            login()
+
+def addToWatchList(movie):
+    # Retrieve the list of users
+    users_list = db.get("usersList")
+
+    # Find the user and append a new movie to their whattowatch list
+    for user in users_list:
+        if user["username"] == app.username:
+            for movies in user["whattowatch"]:
+                if movies == movie:
+                    user["whattowatch"].remove(movie)
+                    db.set("usersList", users_list)
+                    return
+            user["whattowatch"].append(movie)
+
+    # Save the updated users list back to the database
+    db.set("usersList", users_list)
+    return
+
+def addToWatched(movie, rating):
+    # Retrieve the list of users
+    users_list = db.get("usersList")
+
+    # Find the user and append a new movie to their whattowatch list
+    for user in users_list:
+        if user["username"] == app.username:
+            # for movies in user["watched"]:
+            #     # print(movies)
+            #     # print(movie)
+            #     if movies == movie:
+            #         return
+            user["watched"][movie] = rating
+
+    # Save the updated users list back to the database
+    db.set("usersList", users_list)
+    return
+
+def login():
+    # print(app.username)
+    for x in db.getall():
+        users = db.get(x)
+        usernamelist = []
+        for y in users:
+            usernamelist.append(y.get("username"))
+        if (app.username in usernamelist):
+            for z in users:
+                if (app.username == z.get("username")):
+                    if (app.password == z.get("password")):
+                        # print("Logged in!")
+                        # print(app.username)
+                        app.watched = z.get("watched")
+                        # print(app.watched)
+                        app.watchlist = z.get("whattowatch")
+                        app.loggedin = True
+                    else:
+                        print("Password is incorrect")
+        else:
+            print("User does not exist")
+
+@app.route("/")
+def home():
+    return flask.send_from_directory(app.static_folder, "signin.html")
+
+@app.route('/get-watched')
+def get_watched():
+    return flask.jsonify(app.watched)
+
+@app.route('/get-watchlist')
+def get_watchlist():
+    return flask.jsonify(app.watchlist)
+
+@app.post('/add-to-watchlist')
+def add_to_watchlist():
+    post_data = flask.request.form
+    addToWatchList(post_data.get("movie"))
+
+    data=json.dumps( app.loggedin )
+    
+    return flask.Response(data,
+                          status=200,
+                          headers={
+                              "Content-Type":"application/json"
+                          }
+                          )
+
+@app.post('/add-to-watched')
+def add_to_watched():
+    post_data = flask.request.form
+    addToWatched(post_data.get("movie"), post_data.get("rating"))
+
+    data=json.dumps( app.loggedin )
+    
+    return flask.Response(data,
+                          status=200,
+                          headers={
+                              "Content-Type":"application/json"
+                          }
+                          )
+
+
+@app.post("/API/LOGIN")
+def handle_login():
+    post_data = flask.request.form
+    
+    app.username = post_data.get("username")
+    app.password = post_data.get("password")
+    app.loggedin = post_data.get("loggedin")
+
+    login()
+
+    data=json.dumps( app.loggedin )
+    
+    return flask.Response(data,
+                          status=200,
+                          headers={
+                              "Content-Type":"application/json"
+                          }
+                          )
+
+@app.post("/API/SIGNUP")
+def handle_signup():
+    # print("/API/SIGNUP invoked, post_data:")
+    post_data = flask.request.form
+    # print("Post data: ", post_data)
+    
+    app.username = post_data.get("username")
+    app.password = post_data.get("password")
+
+    signup()
+
+    data=json.dumps( app.loggedin )
+    
+    return flask.Response(data,
+                          status=200,
+                          headers={
+                              "Content-Type":"application/json"
+                          }
+                          )
+
+@app.get("/shutdown")
+def shutdown():
+    print("Shutting down the server")
+    os._exit(0)
+
+# TODO - Change Port to an appropriate individual port for yourself
+app.run(host='0.0.0.0', port=10470)
---
+++ public/scripts/scripts.js
@@ -0,0 +1,322 @@
+username = null
+movies = []
+movietitle = null
+moviegenre = null
+movieyear = null
+movieplot = null
+movieposter = null
+loggedin = false
+
+window.addEventListener("load", (event) => {    
+    loadvars()
+    console.log("page is fully loaded")
+    console.log("sign in status: ", loggedin)
+});
+
+
+function opensignin(){
+    document.querySelector("#signin").addEventListener("click", () => {
+        let usernameString = document.querySelector("#username").value
+        let passwordString = document.querySelector("#password").value
+        signIn(usernameString, passwordString)
+    })
+}
+
+function opensignup(){
+    document.querySelector("#createAccount").addEventListener("click", () => {
+        let usernameString = document.querySelector("#newUsername").value
+        let passwordString = document.querySelector("#newPassword").value
+        signUp(usernameString, passwordString)
+    })
+}
+
+function openmymovies(){
+    document.querySelector(".signout").addEventListener("click", () => {
+        loggedin = false
+        savevars()
+        console.log("signed out")
+    })
+
+    const movieList = document.querySelector("#moviesList")
+
+    async function fetchMovies() {
+        try {
+            const response = await fetch('/get-watched');
+            const data = await response.json();
+            console.log("data: ", data);
+    
+            for (const movie in data) {
+                const image = document.createElement('img');
+                const listItem = document.createElement('li');
+    
+                const posterUrl = await returnPosterByTitle(movie);
+                console.log(posterUrl);
+    
+                if (posterUrl) {
+                    image.src = posterUrl;
+                } else {
+                    image.src = "../noimage.png";
+                }
+    
+                image.alt = `${movie} image`
+                image.style.width = '30px'
+                image.style.height = '30px'
+                image.style.borderRadius = '50%'
+                image.style.border = '1px solid black'
+    
+                listItem.appendChild(image);
+    
+                const textNode = document.createTextNode(`   ${movie}; ${data[movie]} stars`);
+                listItem.appendChild(textNode);
+    
+                listItem.addEventListener("click", () => { fetchMovieByTitle(movie) });
+    
+                movieList.appendChild(listItem);
+            }
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+        }
+    }
+    
+    fetchMovies();
+}
+
+function openwatchlist(){
+    document.querySelector(".signout").addEventListener("click", () => {
+        loggedin = false
+        savevars()
+        console.log("signed out")
+    })
+
+    const movieList = document.querySelector("#moviesWatchList")
+
+    async function fetchMovies() {
+        try {
+            const response = await fetch('/get-watchlist');
+            const data = await response.json();
+            console.log("data: ", data);
+    
+            for (const movie in data) {
+                const image = document.createElement('img');
+                const listItem = document.createElement('li');
+    
+                const posterUrl = await returnPosterByTitle(data[movie]);
+                console.log(posterUrl);
+    
+                if (posterUrl) {
+                    image.src = posterUrl;
+                } else {
+                    image.src = "../noimage.png";
+                }
+    
+                image.alt = `${data[movie]} image`;
+                image.style.width = '30px';
+                image.style.height = '30px';
+                image.style.borderRadius = '50%';
+                image.style.border = '1px solid black';
+    
+                listItem.appendChild(image);
+    
+                const textNode = document.createTextNode("   " + data[movie]);
+                listItem.appendChild(textNode);
+    
+                listItem.addEventListener("click", () => { fetchMovieByTitle(data[movie]) });
+    
+                movieList.appendChild(listItem);
+            }
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+        }
+    }
+    
+    fetchMovies();
+}
+
+function opensearch(){
+    document.querySelector("#search").addEventListener("click", () => {
+        let searchedmovie = document.querySelector("#searchbox").value
+        fetchMovieByTitle(searchedmovie)
+    })
+}
+
+function openmovie(){
+    document.querySelector("#movie-title").innerHTML = movietitle;
+    document.querySelector("#movie-genre").innerHTML = moviegenre;
+    document.querySelector("#movie-plot").innerHTML = movieplot;
+    document.querySelector("#movie-year").innerHTML = movieyear;
+    console.log(movieposter)
+    document.querySelector("#movie-poster").src = movieposter;
+    document.querySelector("#addToList").addEventListener("click", () => {
+        addToWatchList(username, movietitle)
+    })
+
+    const stars = document.querySelectorAll('.image-button');
+
+    stars.forEach((star, index) => {
+      star.addEventListener('mouseenter', () => {
+        for (let i = 0; i <= index; i++) {
+          stars[i].classList.add('gold');
+        }
+      });
+
+      star.addEventListener('mouseleave', () => {
+        stars.forEach(star => star.classList.remove('gold'));
+      });
+
+      star.addEventListener("click", () => {
+        addToWatched(username, movietitle, index + 1)
+      })
+    });
+}
+
+async function addToWatchList(user, title){
+    let currentData = {"username": user, "movie": title}
+    const urlEncodedData = new URLSearchParams(currentData).toString();
+
+    let response = await fetch("/add-to-watchlist", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        },
+        body: urlEncodedData
+        }
+    )
+
+    let responseText = await response.text()
+    console.log(responseText);
+    
+    if (responseText === "true"){
+        document.location.href = "watchlist.html"
+    }
+}
+
+async function addToWatched(user, title, rating){
+    let currentData = {"username": user, "movie": title, "rating": rating}
+    const urlEncodedData = new URLSearchParams(currentData).toString();
+
+    let response = await fetch("/add-to-watched", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        },
+        body: urlEncodedData
+        }
+    )
+} 
+
+async function signIn(name, pass) {
+    let currentData = {"username": name, "password": pass}
+    const urlEncodedData = new URLSearchParams(currentData).toString();
+
+    let response = await fetch("/API/LOGIN", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        },
+        body: urlEncodedData
+        }
+    )
+
+    let responseText = await response.text()
+    console.log(responseText);
+    
+    if (responseText === "true"){
+        console.log("SIGN IN TRUE")
+        loggedin = true
+        savevars()
+        document.location.href = "mymovies.html"
+    }
+}
+
+async function signUp(name, pass) {
+    let currentData = {"username": name, "password": pass, "loggedin": loggedin}
+    const urlEncodedData = new URLSearchParams(currentData).toString();
+
+    let response = await fetch("/API/SIGNUP", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        },
+        body: urlEncodedData
+        }
+    )
+
+    let responseText = await response.text()
+    console.log(responseText);
+    
+    if (responseText === "true"){
+        document.location.href = "mymovies.html"
+    }
+}
+
+async function returnPosterByTitle(title) {
+    const apiKey = '10707b46'
+    const url = `http://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.Response === "True") {
+            return data.Poster;
+        } else {
+            console.log("Error:", data.Error);
+            return false
+        }
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+    }
+    return false
+}
+
+async function fetchMovieByTitle(title) {
+    const apiKey = '10707b46'
+    const url = `http://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${apiKey}`;
+    const posterUrl = `http://img.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.Response === "True") {
+            console.log("Movie Title:", data.Title);
+            console.log("Year:", data.Year);
+            console.log("Plot:", data.Plot);
+            console.log("Poster:", data.Poster);
+            console.log("Ratings:", data.Ratings);
+            movietitle = data.Title;
+            movieyear = data.Year;
+            movieplot = data.Plot;
+            moviegenre = data.Genre;
+            movieposter = data.Poster;
+            savevars()
+            document.location.href = "movie.html"
+        } else {
+            console.log("Error:", data.Error);
+            
+        }
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+    }
+
+}
+
+function savevars(){
+    localStorage.setItem('loggedin', loggedin)
+    localStorage.setItem('movietitle', movietitle)
+    localStorage.setItem('moviegenre', moviegenre)
+    localStorage.setItem('movieyear', movieyear)
+    localStorage.setItem('movieplot', movieplot)
+    localStorage.setItem('movieposter', movieposter)
+    console.log("vars saved: ", loggedin, movietitle, moviegenre, movieyear, movieplot, movieposter)
+}
+
+function loadvars(){
+    loggedin = localStorage.getItem('loggedin')
+    movietitle = localStorage.getItem('movietitle')
+    moviegenre = localStorage.getItem('moviegenre')
+    movieyear = localStorage.getItem('movieyear')
+    movieplot = localStorage.getItem('movieplot')
+    movieposter = localStorage.getItem('movieposter')
+    console.log("vars loaded: ", loggedin, movietitle, moviegenre, movieyear, movieplot, movieposter)
+}
